9bb8b87d0f7b116469602a5730c4b42d105d7ace
Matias Luraghi <matiaslgh@gmail.com>
2023-06-03 11:58:55 +0200

    fix: support for dark and light mode on the side bar and their pages

--- frontend/src/app/[companyId]/NavBar.tsx
+++ frontend/src/app/[companyId]/NavBar.tsx
@@ -44,7 +44,9 @@ export default async function NavBar({ companyId }: NavBarProps) {
     >
       <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
         <div className="flex items-center pl-2.5 mb-7">
-          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">{company.name}</span>
+          <span className="self-center text-xl font-semibold whitespace-nowrap text-black dark:text-white">
+            {company.name}
+          </span>
         </div>
         <ul className="space-y-2 font-medium">
           {navigation.map(item => (
@@ -52,15 +54,15 @@ export default async function NavBar({ companyId }: NavBarProps) {
               <Link
                 href={item.href}
                 className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 ${
-                  item.current ? 'bg-gray-900' : ''
+                  item.current ? 'bg-gray-100 dark:bg-gray-700' : ''
                 }`}
               >
                 <item.icon
                   aria-hidden="true"
-                  className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                  className="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                  fill="currentColor"
                 />
-
-                <span className="flex-1 ml-3 whitespace-nowrap">{item.name}</span>
+                <span className="ml-3">{item.name}</span>
               </Link>
             </li>
           ))}
