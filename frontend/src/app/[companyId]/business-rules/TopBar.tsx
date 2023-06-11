1a2e0fba1e5d2faa4af064facd71766082d8c3ca
Matias Luraghi <matiaslgh@gmail.com>
2023-06-11 11:42:04 +0200

    feat: add top bar to business-rules page and add button

---
+++ frontend/src/app/[companyId]/business-rules/TopBar.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { MouseEventHandler } from 'react';
+
+interface TopBarProps {
+  companyId: string;
+}
+
+export default function TopBar({ companyId }: TopBarProps) {
+  const onSave: MouseEventHandler<HTMLButtonElement> = event => {
+    event.preventDefault();
+    console.log('Click');
+  };
+
+  return (
+    <div className="fixed top-0 z-40 w-full pl-4 pt-2 bg-gray-100 border-gray-200 dark:bg-gray-900">
+      <div className="relative overflow-hidden inline-block">
+        <button
+          onClick={onSave}
+          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+        >
+          Save
+        </button>
+      </div>
+    </div>
+  );
+}
--- frontend/src/app/[companyId]/business-rules/page.tsx
+++ frontend/src/app/[companyId]/business-rules/page.tsx
@@ -2,10 +2,17 @@
 
 import { ReactSortable } from 'react-sortablejs';
 import { BusinessRule } from '@/app/types';
-import { useState } from 'react';
+import { ChangeEventHandler, useState } from 'react';
 import { ChevronUpDownIcon } from '@heroicons/react/24/outline';
+import TopBar from './TopBar';
 
-export default function BusinessRulesPage() {
+interface BusinessRulesPageProps {
+  params: {
+    companyId: string;
+  };
+}
+
+export default function BusinessRulesPage({ params: { companyId } }: BusinessRulesPageProps) {
   const [businessRules, setBusinessRules] = useState<BusinessRule[]>([
     {
       id: 1,
@@ -34,53 +41,70 @@ export default function BusinessRulesPage() {
       minWeight: 10,
     },
   ]);
+
+  // TODO: Fix
+  const addRule = () => setBusinessRules(previous => [{ id: 5, name: 'asd', targetDepartment: 'asd' }, ...previous]);
+
   return (
-    <div className="p-4 sm:ml-64">
-      <ReactSortable list={businessRules} setList={setBusinessRules} animation={200} handle={'.draggable'}>
-        {businessRules.map(rule => (
-          <div
-            key={rule.id}
-            className="relative block  mb-2 max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
-          >
-            <ChevronUpDownIcon className="draggable absolute top-1 left-1 w-5 h-5" fill="currentColor" />
-            <table>
-              <tr>
-                <td className="pl-2 pr-2">Price</td>
-                <td className="pl-2 pr-2">
-                  <Input placeholder="Min" value={rule.minValue} />
-                </td>
-                <td className="pl-2 pr-2">
-                  <Input placeholder="Max" value={rule.maxValue} />
-                </td>
-              </tr>
-              <tr>
-                <td className="pl-2 pr-2">Weight</td>
-                <td className="pl-2 pr-2">
-                  <Input placeholder="Min" value={rule.minWeight} />
-                </td>
-                <td className="pl-2 pr-2">
-                  <Input placeholder="Max" value={rule.maxWeight} />
-                </td>
-              </tr>
-            </table>
-          </div>
-        ))}
-      </ReactSortable>
+    <div className="sm:ml-64">
+      <TopBar companyId={companyId} />
+      <div className="mt-16 p-4">
+        <button
+          type="button"
+          onClick={addRule}
+          className="flex flex-col items-center justify-center w-96 h-36 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+        >
+          Add rule
+        </button>
+
+        <ReactSortable list={businessRules} setList={setBusinessRules} animation={200} handle={'.draggable'}>
+          {businessRules.map(rule => (
+            <div
+              key={rule.id}
+              className="relative block mt-2 max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+            >
+              <ChevronUpDownIcon className="draggable cursor-grab absolute top-1 left-1 w-5 h-5" fill="currentColor" />
+              <table>
+                <tr>
+                  <td className="pl-2 pr-2">Price</td>
+                  <td className="pl-2 pr-2">
+                    <Input placeholder="Min" value={rule.minValue} onChange={() => {}} />
+                  </td>
+                  <td className="pl-2 pr-2">
+                    <Input placeholder="Max" value={rule.maxValue} onChange={() => {}} />
+                  </td>
+                </tr>
+                <tr>
+                  <td className="pl-2 pr-2">Weight</td>
+                  <td className="pl-2 pr-2">
+                    <Input placeholder="Min" value={rule.minWeight} onChange={() => {}} />
+                  </td>
+                  <td className="pl-2 pr-2">
+                    <Input placeholder="Max" value={rule.maxWeight} onChange={() => {}} />
+                  </td>
+                </tr>
+              </table>
+            </div>
+          ))}
+        </ReactSortable>
+      </div>
     </div>
   );
 }
 
 interface InputProps {
+  onChange: ChangeEventHandler<HTMLInputElement>;
   value?: string | number;
   placeholder: string;
 }
-function Input({ value, placeholder }: InputProps) {
+function Input({ value, placeholder, onChange }: InputProps) {
   return (
     <input
       type="text"
       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full m-1 p-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
       placeholder={placeholder}
       value={value}
+      onChange={onChange}
     />
   );
 }
